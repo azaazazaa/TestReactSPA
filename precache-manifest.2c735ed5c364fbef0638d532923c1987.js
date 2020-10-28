73bb243088c2df48463fb2ec6c7f600fcaa7e382
azaazazaa <vitruviy2100@gmail.com>
2020-10-29 03:09:52 +0500

    Updates

---
+++ precache-manifest.2c735ed5c364fbef0638d532923c1987.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe672fb564353e930b83df34a2a4dcf5",
+    "url": "/TestReactSPA/index.html"
+  },
+  {
+    "revision": "887423765fd05b5de411",
+    "url": "/TestReactSPA/static/css/main.9d3b010a.chunk.css"
+  },
+  {
+    "revision": "d8d91dbe795a0b910193",
+    "url": "/TestReactSPA/static/js/2.b8dc4557.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/TestReactSPA/static/js/2.b8dc4557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887423765fd05b5de411",
+    "url": "/TestReactSPA/static/js/main.1f4fc152.chunk.js"
+  },
+  {
+    "revision": "e0dee447ded44d07c590",
+    "url": "/TestReactSPA/static/js/runtime-main.4278159f.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/TestReactSPA/static/media/logo.d5ab31ce.svg"
+  }
+]);
